b080ee0b5798a49d8cace240fbacf8ddd6da63ca
Brice Tran <brice1992@msn.com>
2022-08-08 13:17:08 -0400

    first commit, data set up

---
+++ public/rank2.js
@@ -0,0 +1,119 @@
+// (rates['item'].reboot)
+export const rank2 = {
+    "Ring of Restraint": {
+        reboot: 0.0389864,
+        rebootNoMana: 0.0422476
+    },
+    "Weapon Jump": {
+        reboot: 0.0202730,
+        rebootNoMana: 0.0219688
+    },
+    "Ultimatum Ring": {
+        reboot: 0.0202730,
+        rebootNoMana: 0.0219688
+    },
+    "Risk Taker Ring": {
+        reboot: 0.0202730,
+        rebootNoMana: 0.0219688
+    },
+    "Totalling Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Critical Damage Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Crisis HM Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Tower Boost Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Cleansing Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Overdrive Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Level Jump Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Health Cut Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Critical Defense Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0.0211238
+    },
+    "Limit Ring": {
+        reboot: 0.0194931,
+        rebootNoMana: 0
+    },
+    "Durability Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Clean Defense Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Berserker Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Mana Cut Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0
+    },
+    "Crisis H Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0
+    },
+    "Crisis M Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0
+    },
+    "Critical Shift Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Stance Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Clean Stance Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Swift Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Reflective Ring": {
+        reboot: 0.0191033,
+        rebootNoMana: 0.0207013
+    },
+    "Ocean Glow Earrings": {
+        reboot: 0.0038986,
+        rebootNoMana: 0.0042248
+    },
+    "5 Broken Box Pieces": {
+        reboot: 0.2729046,
+        rebootNoMana: 0.2957331
+    },
+    "5x Oz Point Pouches": {
+        reboot: 0.1091619,
+        rebootNoMana: 0.1182932
+    },
+    "3x Double Experience Coupons (15 minutes)": {
+        reboot: 0.1091619,
+        rebootNoMana: 0.1182932
+    },
+};
