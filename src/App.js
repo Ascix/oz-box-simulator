1439e8033d2ab2d4fbad995759e3a9a010057cb2
Brice Tran <brice1992@msn.com>
2022-08-18 15:22:25 -0400

    moved box ui to the top, rank 1 data added,  simulator adjusted to work with settings

--- src/App.js
+++ src/App.js
@@ -1,170 +1,180 @@
-import { useState } from 'react';
-import { ProgressBar } from 'react-bootstrap';
-import './App.css';
+import { useState } from "react";
+import { Card, ProgressBar } from "react-bootstrap";
+import "./App.css";
+import { Rank1 } from "./components/Rank1";
+import { Rank2 } from "./components/Rank2";
 
 function App() {
-  const Rank2 = [
-    {item: "Berserker Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Broken Box Pieces", reboot: 0.2729046, rebootNoMana: 0.2957331},
-    {item: "Clean Defense Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Cleansing Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Clean Stance Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Crisis HM Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Crisis H Ring", reboot: 0.0191033, rebootNoMana: 0},
-    {item: "Crisis M Ring", reboot: 0.0191033, rebootNoMana: 0},
-    {item: "Critical Damage Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Critical Defense Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Critical Shift Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Durability Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Double Experience Coupons", reboot: 0.1091619, rebootNoMana: 0.1182932},
-    {item: "Health Cut Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Level Jump Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Limit Ring", reboot: 0.0194931, rebootNoMana: 0},
-    {item: "Mana Cut Ring", reboot: 0.0191033, rebootNoMana: 0},
-    {item: "Ocean Glow Earrings", reboot: 0.0038986, rebootNoMana: 0.0042248},
-    {item: "Overdrive Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Oz Point Pouches", reboot: 0.1091619, rebootNoMana: 0.1182932},
-    {item: "Reflective Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Ring of Restraint", reboot: 0.0389864, rebootNoMana: 0.0422476},
-    {item: "Risk Taker Ring", reboot: 0.020273, rebootNoMana: 0.0219688},
-    {item: "Stance Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Swift Ring", reboot: 0.0191033, rebootNoMana: 0.0207013},
-    {item: "Totalling Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Tower Boost Ring", reboot: 0.0194931, rebootNoMana: 0.0211238},
-    {item: "Ultimatum Ring", reboot: 0.020273, rebootNoMana: 0.0219688},
-    {item: "Weapon Jump", reboot: 0.020273, rebootNoMana: 0.0219688},
-];
+  const [rank, setRank] = useState("Rank1");
+  const [mana, setMana] = useState("Yes");
+  const [text, setText] = useState(null);
+  const [loading, setLoading] = useState("100");
+  const [disabled, setDisabled] = useState(false);
+  const [item, setItem] = useState("Rank1");
+  const [rolled, setRolled] = useState(null);
 
-function GenerateRing() {
-    let totalChance = 0
-    for (let i = 0; i < Rank2.length; i++) {
-      totalChance += Rank2[i].reboot;
-    }
-      let RNG = Math.random() * totalChance
-      let currentTotal = 0
-      let prize = Rank2[0].item
-
-      for (let i = 0; i < Rank2.length; i++) {
-        currentTotal += Rank2[i].reboot
-        if (RNG < currentTotal) {
-          prize = Rank2[i].item
-          break
+  function GenerateRing() {
+    let totalChance = 0;
+    
+    if (rank === "Rank1") {
+      if (mana === "Yes") {
+        for (let i = 0; i < Rank1.length; i++) {
+          totalChance += Rank1[i].reboot;
         }
+        let RNG = Math.random() * totalChance;
+        let currentTotal = 0;
+        let prize = Rank2[0].item;
+        for (let i = 0; i < Rank1.length; i++) {
+          currentTotal += Rank1[i].reboot;
+          if (RNG < currentTotal) {
+            prize = Rank1[i].item;
+            break;
+          }
+        }
+        return prize;
+      } else {
+        for (let i = 0; i < Rank1.length; i++) {
+          totalChance += Rank1[i].noMana;
+        }
+        let RNG = Math.random() * totalChance;
+        let currentTotal = 0;
+        let prize = Rank2[0].item;
+        for (let i = 0; i < Rank1.length; i++) {
+          currentTotal += Rank1[i].noMana;
+          if (RNG < currentTotal) {
+            prize = Rank1[i].item;
+            break;
+          }
+        }
+        return prize;
       }
-      return prize
     }
-  GenerateRing()
+    if (rank === "Rank2") {
+      if (mana === "Yes") {
+        for (let i = 0; i < Rank2.length; i++) {
+          totalChance += Rank2[i].reboot;
+        }
+        let RNG = Math.random() * totalChance;
+        let currentTotal = 0;
+        let prize = Rank2[0].item;
+        for (let i = 0; i < Rank2.length; i++) {
+          currentTotal += Rank2[i].reboot;
+          if (RNG < currentTotal) {
+            prize = Rank2[i].item;
+            break;
+          }
+        }
+        return prize;
+      } else {
+        for (let i = 0; i < Rank2.length; i++) {
+          totalChance += Rank2[i].noMana;
+        }
+        let RNG = Math.random() * totalChance;
+        let currentTotal = 0;
+        let prize = Rank2[0].item;
+        for (let i = 0; i < Rank2.length; i++) {
+          currentTotal += Rank2[i].noMana;
+          if (RNG < currentTotal) {
+            prize = Rank2[i].item;
+            break;
+          }
+        }
+        return prize;
+      }
+    }
+  }
+
   function GenerateLevel() {
     const RingLevel = [
-      {level: "1", chance: .41}, 
-      {level: "2", chance: .28}, 
-      {level: "3", chance: .20}, 
-      {level: "4", chance: .11}
-    ]
+      { level: "1", chance: 0.41 },
+      { level: "2", chance: 0.28 },
+      { level: "3", chance: 0.2 },
+      { level: "4", chance: 0.11 },
+    ];
 
-    let totalChance = 0
+    let totalChance = 0;
     for (let i = 0; i < RingLevel.length; i++) {
       totalChance += RingLevel[i].chance;
     }
-    let RNG = Math.random() * totalChance
-    let currentTotal = 0
-    let level = RingLevel[0].level
+    let RNG = Math.random() * totalChance;
+    let currentTotal = 0;
+    let level = RingLevel[0].level;
     for (let i = 0; i < RingLevel.length; i++) {
-      currentTotal += RingLevel[i].chance
+      currentTotal += RingLevel[i].chance;
       if (RNG < currentTotal) {
-        level = RingLevel[i].level
-        break
+        level = RingLevel[i].level;
+        break;
       }
     }
-    return level
+    return level;
   }
-  
+
   // function OpenBox() {
 
   // }
-  const [rank, setRank] = useState("2");
-  const [text, setText] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [disabled, setDisabled] = useState(false)
-  const [item, setItem] = useState("Rank2")
-  const [rolled, setRolled] = useState(null)
-  
-  let progress = 0
+
+  let progress = 0;
   function Progress() {
     //4s
     setTimeout(() => {
       if (progress < 100) {
-        progress ++
-        setLoading(progress)
-        Progress()
+        progress++;
+        setLoading(progress);
+        Progress();
       } else {
-        setDisabled(false)
+        setDisabled(false);
       }
-    }, 30)
+    }, 30);
   }
-  
-  const handleOpen = () => {
-    setLoading(null)
-    setText(null)
-    setDisabled(true)
-    setTimeout(() => {
-      Progress()
-    }, 450)
-    const reward = GenerateRing()
-    let level = ""
-    if (reward === 'Broken Box Pieces' || reward === 'Oz Point Pouches' || reward === 'Ocean Glow Earrings' || reward === 'Double Experience Coupons') {
 
+  const handleOpen = () => {
+    setLoading(null);
+    setText(null);
+    setDisabled(true);
+    setTimeout(() => {
+      Progress();
+    }, 450);
+    const reward = GenerateRing();
+    let level = "";
+    if (
+      reward === "Broken Box Pieces" ||
+      reward === "Oz Point Pouches" ||
+      reward === "Ocean Glow Earrings" ||
+      reward === "Double Experience Coupons"
+    ) {
     } else {
-      level = ` (${GenerateLevel()})`
+      level = ` (${GenerateLevel()})`;
     }
     setTimeout(() => {
-      setItem(reward.replace(/ /g, ''))
-      setText(reward + level)
-      setRolled(true)
-    }, 4000)
-  }
+      setItem(reward?.replace(/ /g, ""));
+      setText(reward + level);
+      setRolled(true);
+    }, 3900);
+  };
 
   const handleReset = () => {
-    setItem("Rank2")
-    setRank("2")
-    setText(null)
-    setLoading(null)
-    setRolled(null)
-  }
+    setItem(rank);
+    setText(null);
+    setRolled(null);
+  };
+
+  const handleRank = (e) => {
+    setRank(e.target.value);
+    setItem(e.target.value);
+  };
+
+  const handleMana = (e) => {
+    setMana(e.target.value);
+  };
 
   return (
     <div className="App">
-      <h1>Oz Ring Simulator</h1>
-      <form>
-        <div className="server">
-          <h2>What server do you play on?</h2>
-              <input type="radio" id="reboot" name="server" value="Reboot" defaultChecked/>
-              <label htmlFor="reboot">Reboot</label>
-              <input type="radio" id="regular" name="server" value="Regular" disabled/>
-              <label htmlFor="regular">Regular</label>
-          </div>
-        <div className="manaRadios">
-          <h2>Does your class use mana?</h2>
-              <input type="radio" id="mana-yes" name="mana_or_none" value="Yes" defaultChecked/>
-              <label htmlFor="mana-yes">Yes</label>
-              <input type="radio" id="mana-no" name="mana_or_none" value="No"/>
-              <label htmlFor="mana-no">No</label>
-        </div>
-        <div className="rankBox">
-          <h2>What box do you want to open?</h2>
-              <input type="radio" id="box-1" name="boxRank" value="1" defaultChecked/>
-              <label htmlFor="box-1">Rank 1</label>
-              <input type="radio" id="box-2" name="boxRank" value="2"/>
-              <label htmlFor="box-2">Rank 2</label>
-        </div>
-      </form>
+      <div className="title">Oz Ring Simulator</div>
       <div className="box-ui">
-          <div className="box-title">
-            ALICIA'S BOX
-          </div>
+        <div className="box-title">ALICIA'S BOX</div>
         <div className="box">
           <div className="opening">
-            {text ? text : `OPENING A RANK ${rank} BOX`}
+            {text ? text : `OPENING A RANK ${rank.charAt(4)} BOX`}
           </div>
           <div className="progress-bar">
             <ProgressBar variant="danger" now={loading} />
@@ -173,15 +183,98 @@ function GenerateRing() {
             <img src={`/items/${item}.png`} alt="ring"></img>
           </div>
           <div className="box-buttons">
-            {
-              rolled ? <button className="ok" onClick={handleReset}>OK</button> : <>
-            <button className={ disabled ? "open disabled" : "open" } onClick={handleOpen} disabled={disabled}>OPEN</button>
-            <button className="cancel" onClick={handleReset}>CANCEL</button>
+            {rolled ? (
+              <button className="ok" onClick={handleReset}>
+                OK
+              </button>
+            ) : (
+              <>
+                <button
+                  className={disabled ? "open disabled" : "open"}
+                  onClick={handleOpen}
+                  disabled={disabled}
+                >
+                  OPEN
+                </button>
+                <button className="cancel" onClick={handleReset}>
+                  CANCEL
+                </button>
               </>
-            }
+            )}
           </div>
         </div>
       </div>
+      <Card className="d-flex mx-auto form" style={{ width: "50vw" }}>
+        <form>
+          <h2>Settings</h2>
+          <div className="server">
+            <h4>Server</h4>
+            <div className="radios">
+              <input
+                type="radio"
+                id="reboot"
+                name="server"
+                value="Reboot"
+                defaultChecked
+                disabled={disabled}
+              />
+              <label htmlFor="reboot">Reboot</label>
+              <input
+                type="radio"
+                id="regular"
+                name="server"
+                value="Regular"
+                disabled={true}
+              />
+              <label htmlFor="regular">Regular</label>
+            </div>
+          </div>
+          <div className="manaRadios" onChange={handleMana}>
+            <h4>Mana class?</h4>
+            <div className="radios">
+              <input
+                type="radio"
+                id="yesMana"
+                name="mana_or_none"
+                value="Yes"
+                defaultChecked
+                disabled={disabled}
+              />
+              <label htmlFor="yesMana">Yes</label>
+              <input
+                type="radio"
+                id="noMana"
+                name="mana_or_none"
+                value="No"
+                disabled={disabled}
+              />
+              <label htmlFor="noMana">No</label>
+            </div>
+          </div>
+          <div className="rankRadios" onChange={handleRank}>
+            <h4>Box Rank</h4>
+            <div className="radios">
+              <input
+                type="radio"
+                id="box-1"
+                name="boxRank"
+                value="Rank1"
+                defaultChecked
+                disabled={disabled}
+              />
+              <label htmlFor="box-1">Rank 1</label>
+              <input
+                type="radio"
+                id="box-2"
+                name="boxRank"
+                value="Rank2"
+                disabled={disabled}
+              />
+              <label htmlFor="box-2">Rank 2</label>
+            </div>
+            </div>
+        </form>
+      </Card>
     </div>
   );
 }
